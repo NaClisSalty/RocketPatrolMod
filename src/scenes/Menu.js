1bd0e7dc3492687ef2eb326985d753dcf4f52613
Ethan Salzman <ezsalzma@ucsc.edu>
2020-04-17 15:45:20 -0700

    implemented beter audio that doesnt kill my ears

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -6,9 +6,12 @@ class Menu extends Phaser.Scene {
 
     preload(){
         //load audio
-        this.load.audio('sfx_select', './assets/blip_select12.wav');
-        this.load.audio('sfx_explosion', './assets/explosion38.wav');
-        this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
+        //this.load.audio('sfx_select', './assets/blip_select12.wav');
+        this.load.audio('sfx_select', './assets/beeps.wav');
+        //this.load.audio('sfx_explosion', './assets/explosion38.wav');
+        this.load.audio('sfx_explosion', './assets/betterExplosion.mp3');
+        //this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
+        this.load.audio('sfx_rocket', './assets/betterRocket.wav');
         
     }
 
