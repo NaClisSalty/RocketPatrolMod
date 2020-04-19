4399cf87c476ea52020acfb36356de6c4bb593e2
Ethan Salzman <ezsalzma@ucsc.edu>
2020-04-18 23:15:52 -0700

    changed colors of in game borders for fun

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -19,10 +19,10 @@ class Play extends Phaser.Scene {
         this.starfield = this.add.tileSprite(0,0,640,480,'starfield').setOrigin(0,0);
 
         //white rectalgle boarders
-        this.add.rectangle(5,5,630,32,0xFFFFFF).setOrigin(0,0);
-        this.add.rectangle(5,443,630,32,0xFFFFFF).setOrigin(0,0);
-        this.add.rectangle(5,5,32,455,0xFFFFFF).setOrigin(0,0);
-        this.add.rectangle(603,5,32,455,0xFFFFFF).setOrigin(0,0);
+        this.add.rectangle(5,5,630,32,0x00ffff).setOrigin(0,0);
+        this.add.rectangle(5,443,630,32,0x00ffff).setOrigin(0,0);
+        this.add.rectangle(5,5,32,470,0xff5050).setOrigin(0,0);
+        this.add.rectangle(603,5,32,470,0xff5050).setOrigin(0,0);
 
         //green UI background
         this.add.rectangle(37,42,566,64, 0x00FF00).setOrigin(0,0);
