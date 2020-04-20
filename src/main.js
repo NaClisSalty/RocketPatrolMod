6242e4f8c66585c21535cda4fcd413a2c9257381
Ethan Salzman <ezsalzma@ucsc.edu>
2020-04-19 18:02:58 -0700

    adedd points total

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
+//Ethan Salzman
+//Ponts:
+/*
+1) Added background music to play scene(10)
+2) Player can move rocket after its fired(10)
+3) Displays Time left in seconds on screen (15)
+4) Created a new title screen(15)
+5) Made a new spaceship type thats smaller faster and worth more(25)
+6) Implemented code that adds time to clock for sucsessful hits(25)
+Points: 10+10 = 20+15 = 35+15 = 50+25 =75+25 = 100
+*/
+
 let config = {
     type: Phaser.CANVAS,
     width: 640,
